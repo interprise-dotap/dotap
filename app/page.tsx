a0878ebc03843f74eca300194b4b82b87dcff633
Lucas Vital <lucasvitaldemelo@gmail.com>
2025-01-21 17:15:04 -0300

    test: Test db on prod

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,43 @@
 // File: app/page.tsx
+import { Pool } from '@neondatabase/serverless'
 import { addUser } from './api/users'
 
 export default function Page() {
   console.log('DATABASE_URL:', process.env.DATABASE_URL)
   async function create(formData: FormData) {
     'use server'
+
     // Connect to the Neon database
+
+    const pool = new Pool({ connectionString: process.env.DATABASE_URL! })
+
     const name = formData.get('name')?.toString()
     const email = formData.get('email')?.toString()
     const permission = formData.get('permissions')?.toString()
     const password = formData.get('password')?.toString()
 
     if (name && email && permission && password) {
-      addUser({ name, email, permission, password })
-    }
+      const query = `
+      INSERT INTO users (name, email, permission, password_hash) 
+      VALUES ($1, $2, $3, $4)
+      RETURNING *;
+    `
 
-    console.log('Fununcio')
+      const values = [name, email, permission, password]
+
+      try {
+        const res = await pool.query(query, values)
+        console.log('Usuário inserido com sucesso:', res.rows[0])
+      } catch (err) {
+        console.error('Erro ao inserir usuário:', err)
+      } finally {
+        await pool.end() // Encerra a conexão
+      }
+    }
   }
 
   return (
-    <form action={create} className="flex flex-col w-80 gap-4 text-black">
+    <form action={create} className="flex flex-col w-80 gap-4">
       <input
         className="p-1"
         type="name"
