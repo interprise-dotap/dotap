09264742a0a0a087a9e0c0ff130832960489d363
Lucas Vital <lucasvitaldemelo@gmail.com>
2025-01-21 17:39:29 -0300

    refactor: config drizzdle

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-// File: app/page.tsx
-import { Pool } from '@neondatabase/serverless'
 import { addUser } from './api/users'
 
 export default function Page() {
@@ -7,32 +5,14 @@ export default function Page() {
   async function create(formData: FormData) {
     'use server'
 
-    // Connect to the Neon database
-
-    const pool = new Pool({ connectionString: process.env.DATABASE_URL! })
-
     const name = formData.get('name')?.toString()
     const email = formData.get('email')?.toString()
     const permission = formData.get('permissions')?.toString()
     const password = formData.get('password')?.toString()
 
     if (name && email && permission && password) {
-      const query = `
-      INSERT INTO users (name, email, permission, password_hash) 
-      VALUES ($1, $2, $3, $4)
-      RETURNING *;
-    `
 
-      const values = [name, email, permission, password]
-
-      try {
-        const res = await pool.query(query, values)
-        console.log('Usuário inserido com sucesso:', res.rows[0])
-      } catch (err) {
-        console.error('Erro ao inserir usuário:', err)
-      } finally {
-        await pool.end() // Encerra a conexão
-      }
+      await addUser({ email, name, password, permission })
     }
   }
 
