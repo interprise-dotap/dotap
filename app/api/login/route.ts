5267530e5aae4cbf2a8b6abfeacbe70a593dc22f
Lucas Vital <lucasvitaldemelo@gmail.com>
2025-01-22 14:25:55 -0300

    feat: add token

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -2,6 +2,7 @@ export const dynamic = 'force-static'
 
 import { NextRequest, NextResponse } from "next/server";
 import bcrypt, { genSalt } from 'bcryptjs';
+import jwt from "jsonwebtoken";
 
 import { db } from '@/app/db';
 import { users } from '@/app/db/schema';
@@ -13,19 +14,32 @@ type RequestBody = {
 }
 
 export async function POST(req: NextRequest, res: NextResponse) {
+  const JWT_SECRET = process.env.JWT_SECRET!
 
-  const body: RequestBody = await req.json()
+  const { email, password }: RequestBody = await req.json()
 
   const pepper = process.env.PEPPER;
 
-  const passwordWithPaper = body.password + pepper
+  const passwordWithPaper = password + pepper
+
 
   try {
-    const result = await db.select().from(users).where(eq(users.email, body.email))
+    const result = await db.select().from(users).where(eq(users.email, email))
+
+    const user = result.at(0)
 
     const isMatch = await bcrypt.compare(passwordWithPaper, result[0].passwordHash)
 
-    return NextResponse.json({ authenticated: isMatch }, { status: 201 })
+    if (isMatch && user) {
+      const token = jwt.sign(
+        { id: user.id, permission: user.permission },
+        JWT_SECRET,
+        { expiresIn: "1h" }
+      );
+
+      return NextResponse.json({ token }, { status: 201 })
+    }
+
   } catch (error) {
     console.error('Erro ao realizar login:', error)
     return NextResponse.json({ message: 'fon!' }, { status: 500 })
