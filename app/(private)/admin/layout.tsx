d6fe8437d0108197c65dde4086e2928b18eac478
Giovana Raphaelli <giovanaraphaelli@outlook.com>
2025-01-31 15:29:29 -0300

    refactor: header

--- app/(private)/admin/layout.tsx
+++ app/(private)/admin/layout.tsx
@@ -1,16 +1,21 @@
+'use server';
+
 import Header from '@/components/header';
 import NavBar from '@/components/nav-bar';
+import { getUserInfos } from '@/services/get-user-infos';
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const userInfos = await getUserInfos();
+  const name = userInfos ? userInfos.name : '';
   return (
     <>
-      <Header />
+      <Header name={name} />
       <div className="grid grid-cols-[14rem_1fr] gap-2 h-[calc(100vh-3.6rem)]">
-        <NavBar permission='admin' />
+        <NavBar permission="admin" />
         <main className="container m-auto h-full p-4">{children}</main>
       </div>
     </>
